6371ebcc67e565ff1571850c8f2e8bac09b192dd
shinhs <shs@tigensoft.co.kr>
2025-06-11 16:51:25 +0900

    Day19, 20 학습 - 89/99강 수강 완료

--- react-lecture/section12/src/App.jsx
+++ react-lecture/section12/src/App.jsx
@@ -1,39 +1,106 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { useReducer, useRef, createContext } from "react";
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-function App() {
-  const nav = useNavigate();
+const mockkData = [
+  {
+    id: 1,
+    createdDate: new Date("2025-06-11").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용"
+  },
+  {
+    id: 2,
+    createdDate: new Date("2025-06-10").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용"
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-05-06").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용"
+  },
+]
 
-  const onClickButton = () => {
-    nav('/new');
-  };
+function reducer(state, action) {
+  switch ((action.type)) {
+    case 'CREATE': 
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) => 
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case 'DELETE': 
+      return state.filter((item) => String(item.id) !== String(action.id));  
+    default:
+      return state;
+  }
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mockkData);
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate, 
+        emotionId,
+        content
+      }
+    })
+  }
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch(
+      {
+        type: "UPDATE",
+        data: {
+          id,
+          createdDate, 
+          emotionId,
+          content
+        }
+      }
+    )
+  }
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id
+    })
+  }
  
   return (
     <>
-      <div>
-        {/* 클라이언트 사이드 렌더링: 필요한 컴포넌트만 교체(새로고침X) */}
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <button onClick={onClickButton}>New 페이지로 이동</button>
-      </div>
-      <Routes>
-        {/* 경로에 따라 렌더링될 컴포넌트가 달라짐 */}
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        {/* 나머지 경로 설정 */}
-        <Route path='*' element={<Notfound />} />
-      </Routes>
+      <DiaryStateContext.Provider value = { data }>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }} >
+          <Routes>
+            {/* 경로에 따라 렌더링될 컴포넌트가 달라짐 */}
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            {/* 나머지 경로 설정 */}
+            <Route path='*' element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
--- react-lecture/section12/src/pages/Home.jsx
+++ react-lecture/section12/src/pages/Home.jsx
@@ -1,10 +1,46 @@
-import { useSearchParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+
+const getMonthlyData = (pivotDate, data) => {
+    const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime(); // 1일 0시 0분 0초
+
+    const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1, 0, 23, 59, 59).getTime(); // 0일 -> 이전 달의 마지막날로 설정됨
+
+    return data.filter((item) => 
+        beginTime <= item.createdDate && item.createdDate <= endTime
+    );
+}
 
 const Home = () => {
-    const [params, setParams] = useSearchParams();
-    console.log(params.get("value"))
+    const data = useContext(DiaryStateContext);
+    const [pivotDate, setPivotDate] = useState(new Date());
+    const monthlyData = getMonthlyData(pivotDate, data);
 
-    return <div>Home</div>;
+    const onIncreaseMonth = () => {
+        setPivotDate(
+            new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1)
+        );
+    };
+    const onDecreaseMonth = () => {
+        setPivotDate(
+            new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1)
+        );
+    };
+
+    return (
+        <div>
+            <Header 
+                title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`} 
+                leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+                rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+            />
+            <DiaryList data={ monthlyData } />
+        </div>
+    );
 };
 
 export default Home;
