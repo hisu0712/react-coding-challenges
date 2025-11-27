a99f9b66d06fbedc6ee5ce282068feb15d4fdfac
신희수 <shs@tigensoft.co.kr>
2025-11-27 10:42:43 +0900

    8.13까지 수강 완료

--- react-masterclass-l8/src/App.tsx
+++ react-masterclass-l8/src/App.tsx
@@ -1,33 +1,106 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
-const Circle = styled(motion.div)`
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  background-color: white;
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
+  initial: {
+    opactiy: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    y: 20,
+  },
+};
+
+const box = {
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 0.3,
+    },
+  }),
+};
+
+function App() {
+  const [visible, setVisible] = useState(1);
+  const [isBack, setIsBack] = useState(false);
+  const nextPlease = () => {
+    setIsBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setIsBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+
+  return (
+    <Wrapper>
+      <AnimatePresence exitBeforeEnter custom={isBack}>
+        <Box
+          custom={isBack}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
+    </Wrapper>
+  );
+}
+
+export default App;
+
+/* const boxVariants = {
   start: {
     opacity: 0,
     scale: 0.5,
@@ -55,19 +128,10 @@ const circleVariants = {
     opacity: 1,
     y: 0,
   },
-};
-
-function App() {
-  return (
-    <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
-    </Wrapper>
-  );
-}
-
-export default App;
+}; */
+/* <Box variants={boxVariants} initial="start" animate="end">
+<Circle variants={circleVariants} />
+<Circle variants={circleVariants} />
+<Circle variants={circleVariants} />
+<Circle variants={circleVariants} />
+</Box> */
