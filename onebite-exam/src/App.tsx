eb0faad0c398979475755c5afc1a17295ad826a1
신희수 <shs@tigensoft.co.kr>
2026-05-26 10:13:22 +0900

    feat(리액트 실전): (12/90) 수강

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      {/* 1. 타이포그래피 */}
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
+      <div className="text-lg font-bold">text-lg</div>
+      <div className="text-xl font-extrabold">text-xl</div>
+      <div className="text-[13px]">text-13px</div>
+
+      {/* 2. 백그라운드컬러 */}
+      <div className="bg-amber-500">amber-500</div>
+
+      {/* 3. 사이즈 */}
+      <div className="h-20 w-20 bg-blue-500">box</div>
+
+      {/* 4. 여백 */}
+      <div className="m-5 h-50 w-50 bg-red-400 px-5 pt-5">
+        <div className="h-full w-full bg-violet-500"></div>
+      </div>
+
+      {/* 5. 보더 */}
+      <div className="m-5 rounded-sm border-y-2 border-red-500">border</div>
+
+      {/* 6. 플렉스 컨테이너 */}
+      <div className="flex items-center justify-evenly">
+        <div className="h-10 w-10 border">1</div>
+        <div className="w-10 flex-1 border">2</div>
+        <div className="h-40 w-10 border">3</div>
+        <div className="w-10 border">4</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
