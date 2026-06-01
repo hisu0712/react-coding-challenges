a076bb66b9685937f3ae6640565be9df9529f6da
신희수 <shs@tigensoft.co.kr>
2026-06-01 13:31:50 +0900

    feat(리액트 실전): (29/90) 수강

--- onebite-exam/src/main.tsx
+++ onebite-exam/src/main.tsx
@@ -3,12 +3,26 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      gcTime: 5 * 60 * 1000, // 5분
+
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <App />
     </QueryClientProvider>
   </BrowserRouter>,
