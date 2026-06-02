9a85588012d26cdfb92fbb03d9da0abbb9e800c1
신희수 <shs@tigensoft.co.kr>
2026-06-02 18:17:30 +0900

    feat(리액트 실전): (36/90) 수강

--- onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -12,10 +12,21 @@ export function useCreateTodoMutation() {
     onSettled: () => {},
     onSuccess: (newTodo) => {
       // createTodo 반환값이 매개변수로 제공됨
-      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
-        if (!prevTodos) return [newTodo];
-        return [...prevTodos, newTodo];
-      });
+      queryClient.setQueryData<Todo>(
+        QUERY_KEYS.todo.detail(newTodo.id),
+        newTodo,
+      );
+      queryClient.setQueryData<string[]>(
+        QUERY_KEYS.todo.list,
+        (prevTodoIds) => {
+          if (!prevTodoIds) return [newTodo.id];
+          return [...prevTodoIds, newTodo.id];
+        },
+      );
+      // queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+      //   if (!prevTodos) return [newTodo];
+      //   return [...prevTodos, newTodo];
+      // });
     },
     onError: (error) => {
       window.alert(error.message);
