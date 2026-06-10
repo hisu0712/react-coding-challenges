681b62a0632a18c82273d0376777a014f7b9deeb
신희수 <shs@tigensoft.co.kr>
2026-06-10 11:25:44 +0900

    feat(리액트 실전): (51/90) 수강 & 복습 완료

--- onebite-log/src/root-route.tsx
+++ onebite-log/src/root-route.tsx
@@ -6,19 +6,27 @@ import ResetPasswordPage from "./pages/reset-password-page";
 import IndexPage from "./pages/index-page";
 import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
-import GlobalLayout from "./layout/global-layout";
+import GlobalLayout from "./components/layout/global-layout";
+import GuestOnlyLayout from "./components/layout/guset-only-layout";
+import MemberOnlyLayout from "./components/layout/member-only-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        <Route element={<GuestOnlyLayout />}>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
+
+        <Route element={<MemberOnlyLayout />}>
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        </Route>
+
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
     </Routes>
