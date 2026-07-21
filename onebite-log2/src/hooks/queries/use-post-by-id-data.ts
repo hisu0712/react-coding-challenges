3d97eaecf93973f549bd27a1e50c219609931764
신희수 <shs@tigensoft.co.kr>
2026-07-21 18:12:58 +0900

    feat(리액트 실전): (73/90) 수강 & 복습 완료

--- onebite-log2/src/hooks/queries/use-post-by-id-data.ts
+++ onebite-log2/src/hooks/queries/use-post-by-id-data.ts
@@ -1,5 +1,6 @@
 import { fetchPostById } from "@/api/post";
 import { QUERY_KEYS } from "@/lib/constants";
+import { useSession } from "@/store/session";
 import { useQuery } from "@tanstack/react-query";
 
 export function usePostByIdData({
@@ -9,9 +10,11 @@ export function usePostByIdData({
   postId: number;
   type: "FEED" | "DETAIL";
 }) {
+  const session = useSession();
+
   return useQuery({
     queryKey: QUERY_KEYS.post.byId(postId),
-    queryFn: () => fetchPostById(postId),
+    queryFn: () => fetchPostById({ postId, userId: session!.user.id }),
     enabled: type === "FEED" ? false : true, // 타입이 FEED이면 새로 불러오지 말고 있는 쿼리 써라
   });
 }
